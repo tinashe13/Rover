8747ff40154e5de6165d2f406afcadbbae3ac8d9
Tinashe Dzemwa <tinashedzemwa300@gmail.com>
2023-03-17 22:26:09 +0200

    logic and views now working, left with xUnit test only

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using MarsRover.Models;
+using Microsoft.AspNetCore.Mvc;
+using Rover.Models;
+using System.Diagnostics;
+
+namespace MarsRover.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+ 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(String instruction)
+        {
+            //split the input by each new line
+            string[] message = instruction.Split(Environment.NewLine,StringSplitOptions.RemoveEmptyEntries);
+           
+            //split the input to gee the upper right co-ordinates of the plateau using a character array
+            int[] PlateauArr = message[0].Split(' ').Select(int.Parse).ToArray();
+
+            //create new array to strore the values of the rover starting position and instruction seperate
+            string[] RoverDetails = new string[message.Length - 1];
+            for (int i = 1; i < message.Length; i++)
+            {
+                RoverDetails[i - 1] = message[i];
+            }
+            
+
+            //create an array containing only the starting positions of the Rovers
+            string[] StartingPosition = new string[RoverDetails.Length / 2];
+            for (int i = 0, j = 0; i < RoverDetails.Length; i += 2, j++)
+            {
+                StartingPosition[j] = RoverDetails[i];
+            }
+
+            //create an array containing only the Movement commands for the Rovers
+            string[] RoverInstructions = new string[RoverDetails.Length / 2];
+            for (int i = 1, j = 0; i < RoverDetails.Length; i += 2, j++)
+            {
+                RoverInstructions[j] = RoverDetails[i];
+            }
+
+            var roverResponses = new List<string>();
+            for (int i = 0; i < RoverInstructions.Length; i++)
+            {
+
+                string[] PositionString = StartingPosition[i].Split(' ');
+
+                int xCoodinate = int.Parse(PositionString[0]);
+                int yCoodinate = int.Parse(PositionString[1]);
+                string RoverOrientation = PositionString[2];
+
+                
+                //initiate components
+                var plateau = new Plateau(PlateauArr[0], PlateauArr[1]);
+                var roverPosition = new Position(xCoodinate, yCoodinate, RoverOrientation);
+                var roverInstructions = RoverInstructions[i];
+
+
+                // Explore rover
+                var rover = new Mars_Rover(roverPosition, plateau);
+                rover.ExecuteInstructions(roverInstructions);
+                roverResponses.Add(rover.CurrentPosition.ToString());
+                Console.WriteLine(roverResponses[0]);
+                
+            }
+
+
+
+            // Display responses
+            ViewBag.RoverResponses = roverResponses;
+            return View("index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Rover
-Mars Rover project for interview challenge implemented using C# asp.net latest LTS (long Term Support) version.
